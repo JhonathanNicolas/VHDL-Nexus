1f6a8656229f665c62d7738208e1be258f30920c
Jhonathan Nicolas <jhonathannicolas@gmail.com>
2023-10-10 11:09:40 -0300

    Solved basic logic gates challenge

---
+++ Newer/01 - Basic Logic Gates/Solution/notGate.vhd
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+-- Project: Basic Logic Gates Exercise
+-- Engineer:  Jhonathan Nicolas
+-- Date:   10/10/23
+-- Description: This module contains the implementation for the basic logic 
+--              gates including AND, OR, XOR, and others.
+--------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity notGate is
+    Port ( a : in STD_LOGIC;
+           y : out STD_LOGIC);
+end notGate;
+
+architecture Behavioral of notGate is
+
+begin
+
+y <= not(a);
+
+end Behavioral;
---
+++ Newer/01 - Basic Logic Gates/Solution/xorGate.vhd
@@ -0,0 +1,34 @@
+--------------------------------------------------------------------------------
+-- Project: Basic Logic Gates Exercise
+-- Engineer:  Jhonathan Nicolas
+-- Date:   10/10/23
+-- Description: This module contains the implementation for the basic logic 
+--              gates including AND, OR, XOR, and others.
+--------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xorGate is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           y : out STD_LOGIC);
+end xorGate;
+
+architecture Behavioral of xorGate is
+
+begin
+
+y <= a xor b;
+
+end Behavioral;
